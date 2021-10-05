170ed3471fc0bb84bab93bcedeb1dbd2da0949a9
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 21:21:09 +0300

    task57

--- FCC51-60.js
+++ FCC51-60.js
@@ -77,3 +77,16 @@ function welcomeToBooleans() {
 }
 
 //57
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if (wasThatTrue) {
+  return "Yes, that was true";
+}
+return "No, that was false";
+
+  // Only change code above this line
+
+}
+
+//58
