d74180fd034e0387fa69ca7d830b7a9eaa62b3fc
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-05 16:56:31 +0300

    tasks 26-35

---
+++ FCC29.js
@@ -0,0 +1,5 @@
+// Change code below this line
+
+var someAdjective = "yyaaauu!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
---
+++ FCC30.js
@@ -0,0 +1,8 @@
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+lastName = "Lovelace".length;
+lastNameLength = lastName.length;
+// Only change code below this line
+
+lastNameLength = lastName;
