83283408971bdf18a6fade87121607f34f3171b5
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 21:15:15 +0300

    task55

--- FCC51-60.js
+++ FCC51-60.js
@@ -67,3 +67,13 @@ console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 
 //56
+function welcomeToBooleans() {
+
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+//57
