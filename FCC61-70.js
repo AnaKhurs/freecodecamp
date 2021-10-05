3158ff1ea9ae171d7172637c2eeb644ec22350d0
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 22:02:47 +0300

    task70

--- FCC61-70.js
+++ FCC61-70.js
@@ -123,4 +123,15 @@ function testLogicalOr(val) {
   
   testElse(4);
 
+  //70
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if (val < 5) {
+      return "Smaller than 5";
+    } else {
+    return "Between 5 and 10";
+    }
+  }
   
+  testElseIf(7);
