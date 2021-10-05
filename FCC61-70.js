d8649e4b7426d7b5d2a9ce2c64a3f3186efe90c2
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 21:39:40 +0300

    task64

--- FCC61-70.js
+++ FCC61-70.js
@@ -35,4 +35,19 @@ function testStrictNotEqual(val) {
   
   testGreaterThan(10);
 
+  //64
+  function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val >= 10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
+
   
