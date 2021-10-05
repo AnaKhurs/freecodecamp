2f8a072993b46d0171423aab0a766d6dcf78a145
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-05 17:54:54 +0300

    Create FCC51-60

---
+++ FCC51-60.js
@@ -0,0 +1,16 @@
+//51
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+//52
