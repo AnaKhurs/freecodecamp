573dcc08bf1d89b00d9b8716397dee150a4813fa
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 21:51:22 +0300

    task67

--- FCC61-70.js
+++ FCC61-70.js
@@ -80,4 +80,16 @@ function testStrictNotEqual(val) {
   
   testLessOrEqual(10);
 
+  //67
+  function testLogicalAnd(val) {
+    // Only change code below this line
   
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
