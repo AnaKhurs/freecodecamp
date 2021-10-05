696a3ccc76fe28ca691e61f645d835702652d952
AnaKhurs <ana.khurs@gmail.com>
2021-10-05 21:13:37 +0300

    task55

--- FCC51-60.js
+++ FCC51-60.js
@@ -46,3 +46,24 @@ function processArg(num) {
 
 processed = processArg(7);
 // Only change code below this line
+
+//55
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  var item = arr.shift();
+  return item;
+  // Only change code above this line
+  
+
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+//56
