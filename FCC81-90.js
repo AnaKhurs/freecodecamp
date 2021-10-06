f15aab6095ef328bb03493c69746d6dcef546f6f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 09:53:49 +0300

    Update FCC81-90.js

--- FCC81-90.js
+++ FCC81-90.js
@@ -22,3 +22,16 @@ var testObj = {
   
   var hatValue = testObj.hat;      // Change this line
   var shirtValue = testObj.shirt;    // Change this line
+
+  //83
+  // Setup
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  
+  var entreeValue = testObj["an entree"];   // Change this line
+  var drinkValue = testObj["the drink"];    // Change this line
