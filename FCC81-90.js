a757d5f565be4a786357393f0a80a20f36bf542e
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 11:42:17 +0300

    Update FCC81-90.js

--- FCC81-90.js
+++ FCC81-90.js
@@ -116,3 +116,29 @@ function phoneticLookup(val) {
    }
     // Only change code above this line
   }
+
+  //90
+  var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
