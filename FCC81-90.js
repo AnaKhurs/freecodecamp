fbd25bc84d8018868cf26b54b78dbef4d42aeeea
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 09:46:54 +0300

    Create FCC81-90.js

---
+++ FCC81-90.js
@@ -0,0 +1,11 @@
+//81
+var myDog = {
+    // Only change code below this line
+    "name": "Nik",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Mike", "Ana"]
+    
+    
+    // Only change code above this line
+    };
