32c95ecbb15171bfa6165b5c1aad430a50ee0e22
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 10:54:07 +0300

    Update FCC81-90.js

--- FCC81-90.js
+++ FCC81-90.js
@@ -84,4 +84,24 @@ var myDog = {
   delete myDog.tails;
   // Only change code below this line
 
+  //88
+  // Setup
+function phoneticLookup(val) {
+    var result = "";
   
+    // Only change code below this line
+    var lookup = {
+    alpha:"Adams",
+    bravo:"Boston",
+    charlie:"Chicago",
+    delta:"Denver",
+    echo:"Easy",
+    foxtrot:"Frank"
+  }
+  
+  result = lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
