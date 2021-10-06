48d89eeb0e2a61617150bda92ccb627fffed46ad
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 10:30:57 +0300

    task84

--- FCC81-90.js
+++ FCC81-90.js
@@ -35,3 +35,17 @@ var testObj = {
   
   var entreeValue = testObj["an entree"];   // Change this line
   var drinkValue = testObj["the drink"];    // Change this line
+
+
+  //84!!!!!!
+  // Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  var playerNumber = 16;       // Change this line
+  var player = testObj[playerNumber];   // Change this line
