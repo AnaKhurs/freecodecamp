f5c37f5a2ef2b40dc8202d80b30ab9253f089541
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 11:58:28 +0300

    Update FCC91-100.js

--- FCC91-100.js
+++ FCC91-100.js
@@ -14,3 +14,27 @@ var myStorage = {
 var gloveBoxContents = undefined;
 
 gloveBoxContents = myStorage.car.inside["glove box"];
+
+//92 
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = "";
+
+secondTree = myPlants[1].list[1];
