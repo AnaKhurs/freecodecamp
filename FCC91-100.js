1db254f531114a08b576ce1bb75aab0ba379e242
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 11:53:17 +0300

    Create FCC91-100.js

---
+++ FCC91-100.js
@@ -0,0 +1,16 @@
+//91
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = undefined;
+
+gloveBoxContents = myStorage.car.inside["glove box"];
