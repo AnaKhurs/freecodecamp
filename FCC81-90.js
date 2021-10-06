86597bdf28a788f834216eefa884c5eacab259a8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-06 09:49:51 +0300

    Update FCC81-90.js

--- FCC81-90.js
+++ FCC81-90.js
@@ -9,3 +9,16 @@ var myDog = {
     
     // Only change code above this line
     };
+
+    //82
+    // Setup
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  
+  var hatValue = testObj.hat;      // Change this line
+  var shirtValue = testObj.shirt;    // Change this line
