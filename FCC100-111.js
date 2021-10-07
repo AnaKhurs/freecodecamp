b4d2bdf97e88a2e3ec1154140db80bfbc29f1842
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-07 14:27:38 +0300

    task103

--- FCC100-111.js
+++ FCC100-111.js
@@ -15,3 +15,11 @@ function randomFraction() {
   
     return Math.floor(Math.random() * 10);
   }
+
+
+  //103
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+  }
