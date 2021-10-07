038da43316e1b824f22b4453e6e4ebf230ea663f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-07 14:19:31 +0300

    task102

--- FCC100-111.js
+++ FCC100-111.js
@@ -7,3 +7,11 @@ function randomFraction() {
   
     // Only change code above this line
   }
+
+  //102
+  function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random() * 10);
+  }
