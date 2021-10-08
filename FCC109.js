961a1c1246885e31a6b834d06156bc56a7823ebb
AnaKhurs <ana.khurs@gmail.com>
2021-10-08 21:03:22 +0300

    Create FCC109.js

---
+++ FCC109.js
@@ -0,0 +1,8 @@
+//Use Multiple Conditional (Ternary) Operators
+function checkSign(num) {
+    return (num > 0) ? "positive" 
+    : (num < 0) ? "negative" 
+    : "zero";
+    }
+    
+    checkSign(10);
