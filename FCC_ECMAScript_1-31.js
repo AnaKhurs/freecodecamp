3899b7e517fe1dfbd9689f931948917c41a58b9e
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-11 08:15:16 +0300

    Compare Scopes of the var and let KeywordsPassed

---
+++ FCC_ECMAScript_1-31.js
@@ -0,0 +1,22 @@
+//1
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+//2 Compare Scopes of the var and let KeywordsPassed
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
