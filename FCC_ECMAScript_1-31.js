b6fea07f211d77f1a2a61269ff142c2185330073
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-12 09:44:45 +0300

    Declare a Read-Only Variable with the const Keyword

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -20,3 +20,18 @@ function checkScope() {
   console.log('Function scope i is: ', i);
   return i;
 }
+
+//Declare a Read-Only Variable with the const Keyword
+function printManyTimes(str) {
+
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+
+}
+printManyTimes("freeCodeCamp");
