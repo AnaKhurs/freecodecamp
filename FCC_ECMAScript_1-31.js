4fd21152fe07076add70876380ec0e1743ff310e
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-13 08:23:09 +0300

    5 Prevent Object Mutation

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -53,3 +53,20 @@ s[2] = 7;
 editInPlace();
 
 //5 Prevent Object Mutation
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+
+Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
