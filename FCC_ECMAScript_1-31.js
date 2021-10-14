3bb7c6eb48ab79db449740eb560d9439625be111
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-14 08:18:23 +0300

    Set Default Parameters for Your Functions

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -81,3 +81,11 @@ const myConcat =(arr1, arr2) => {
 };
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+//8 Set Default Parameters for Your Functions
+// Only change code below this line
+const increment = (number , value = 1) => number + value;
+// Only change code above this line
+
+
