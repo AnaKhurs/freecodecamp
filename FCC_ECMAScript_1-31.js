35b7d00f8ad83b1d81f271e6c2290cabcd7507fb
AnaKhurs <ana.khurs@gmail.com>
2021-10-17 08:20:31 +0300

    Write Concise Declarative Functions with ES6

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -127,3 +127,15 @@ const createPerson = (name, age, gender) => {
   return { name, age, gender };
   // Only change code above this line
 };
+
+//19 Write Concise Declarative Functions with ES6
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
