7c6f9c604a6511fdb413036e875d0d412d502d0f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-18 08:36:43 +0300

    Create a JavaScript Promise

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -182,3 +182,8 @@ import subtract from "./math_functions.js";
 // Only change code above this line
 
 subtract(7,4);
+
+//28 Create a JavaScript Promise
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
