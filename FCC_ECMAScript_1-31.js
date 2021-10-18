b716a38111e19ede1aa36f3d7d934cc43341b11e
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-18 08:31:35 +0300

    25 Use * to Import Everything from a File

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -164,3 +164,10 @@ import {uppercaseString, lowercaseString} from "./string_functions.js"
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+//25 Use * to Import Everything from a File
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
