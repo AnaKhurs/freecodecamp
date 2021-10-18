b3f10de00e9a1c70d8dc9aec7394fc7f5e6957bb
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-18 08:44:33 +0300

    Complete a Promise with resolve and reject

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -187,3 +187,15 @@ subtract(7,4);
 const makeServerRequest = new Promise((resolve, reject) => {
 
 });
+
+//29 Complete a Promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
