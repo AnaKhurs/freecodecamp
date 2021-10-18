002f679c52192f5aa613ba099a3def6259a4fcca
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2021-10-18 08:54:56 +0300

    31 Handle a Rejected Promise with catch

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -215,3 +215,23 @@ const makeServerRequest = new Promise((resolve, reject) => {
 makeServerRequest.then(result => {
   console.log(result);
 });
+
+//31 Handle a Rejected Promise with catch
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+});
