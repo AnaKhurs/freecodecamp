3ba13bdb7cae9d3e1d77b15d72cb8d7dc3dedb85
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-18 16:04:31 +0300

    Use Destructuring Assignment to Assign Variables from Nested Objects

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -117,7 +117,18 @@ const {today, tomorrow} = HIGH_TEMPERATURES;
 
 //12 Use Destructuring Assignment to Assign Variables from Objects
 
+//13Use Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
 
+// Only change code below this line
+  
+const { today: {low: lowToday, high: highToday} }  = LOCAL_FORECAST
+
+// Only change code above this line
 
 
 
