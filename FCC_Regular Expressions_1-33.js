7731118fdcd9e229369c077386eacca1a68a4993
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 13:28:01 +0300

    Specify Exact Number of Matches

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -145,3 +145,8 @@ let result = ohRegex.test(ohStr);
 let haStr = "Hazzzzah";
 let haRegex = /Haz{4,}ah/; // Change this line
 let result = haRegex.test(haStr);
+
+//27 Specify Exact Number of Matches
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result = timRegex.test(timStr);
