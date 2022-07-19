36759b5c25ae3fd7a90a5d7ae5428dcaf8bc0dea
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 13:11:00 +0300

    Match Whitespace

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -105,7 +105,28 @@ let noNumRegex = /\D/g; // Change this line
 let result = movieName.match(noNumRegex).length;
 
 //22 Restrict Possible Usernames
+
+/*Usernames are used everywhere on the internet. 
+They are what give users a unique identity on their favorite sites.
+
+You need to check all the usernames in a database. 
+Here are some simple rules that users have to follow when creating their username.
+
+1) Usernames can only use alpha-numeric characters.
+
+2) The only numbers in the username have to be at the end. 
+There can be zero or more of them at the end. Username cannot start with the number.
+
+3) Username letters can be lowercase and uppercase.
+
+4) Usernames have to be at least two characters long. 
+A two-character username can only use alphabet letters as characters.*/
+
 let username = "JackOfAllTrades";
 let userCheck = /^[a-z]([a-z]+\d*|\d{2,})$/i; // Change this line
 let result = userCheck.test(username);
 
+//23 Match Whitespace
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
