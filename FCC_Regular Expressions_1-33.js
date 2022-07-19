7517ac8452dc8dfc9f8ee0f11684b24a675704c8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 11:37:31 +0300

    Find More Than the First Match

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -22,3 +22,8 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = (/coding/); // Change this line
 let result = extractStr.match(codingRegex); // Change this line
+
+//6 Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
