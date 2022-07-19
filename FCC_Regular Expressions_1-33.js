a0e16834e8bb68aba06d3aa9e6d6edceccff078f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 11:50:05 +0300

    Match Numbers and Letters of the Alphabet

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -42,3 +42,8 @@ let result = quoteSample.match(vowelRegex); // Change this line
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
+
+//10 Match Numbers and Letters of the Alphabet
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
