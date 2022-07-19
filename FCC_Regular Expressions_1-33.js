c8598d4811f3d6256b6c0786121471a4e753ff38
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 11:41:17 +0300

    Match Anything with Wildcard Period

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -27,3 +27,8 @@ let result = extractStr.match(codingRegex); // Change this line
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/gi; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
+
+//7 Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
