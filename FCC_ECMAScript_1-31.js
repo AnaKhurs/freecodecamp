f180964c1177a4e9a379b2613d9744ee1424b3a8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 10:03:08 +0300

    Use class Syntax to Define a Constructor Function

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -293,3 +293,15 @@ makeServerRequest.then(result => {
 makeServerRequest.catch(error => {
   console.log(error);
 });
+
+//Use class Syntax to Define a Constructor Function
+// Only change code below this line
+class Vegetable {
+  constructor(name){
+    this.name = name
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
