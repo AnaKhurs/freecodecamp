044f99fab0abfe20ff79a1bfff3aa55dfd026e23
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 11:45:03 +0300

    Match Single Character with Multiple Possibilities

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -32,3 +32,9 @@ let result = twinkleStar.match(starRegex); // Change this line
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result = unRegex.test(exampleStr);
+
+//8 Match Single Character with Multiple Possibilities
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
