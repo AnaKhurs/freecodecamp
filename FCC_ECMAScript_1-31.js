57edf225ba74d8833506f252457ebdfa753356b3
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 10:26:57 +0300

    Use getters and setters to Control Access to an Object

--- FCC_ECMAScript_1-31.js
+++ FCC_ECMAScript_1-31.js
@@ -305,3 +305,23 @@ class Vegetable {
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
+
+//Use getters and setters to Control Access to an Object
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  get temperature() {
+    return 5 / 9 * (this.fahrenheit - 32);
+  }
+  set temperature(celsius) {
+    this.fahrenheit = celsius * 9 / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
