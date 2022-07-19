27db07e204f14813ee23d41b3effcd77c993c1c8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 13:00:54 +0300

    Match All Non-Numbers

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -98,3 +98,8 @@ let result = quoteSample.match(nonAlphabetRegex).length;
 let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g; // Change this line
 let result = movieName.match(numRegex).length;
+
+//21 Match All Non-Numbers
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
