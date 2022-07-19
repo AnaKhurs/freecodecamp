d4297351e9b93d2959ce50727d9fa0c5c3f2e76a
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-19 12:20:49 +0300

    Match Ending String Patterns

--- FCC_Regular Expressions_1-33.js
+++ FCC_Regular Expressions_1-33.js
@@ -78,3 +78,8 @@ let reCriminals = /C+/; // Change this line
 let rickyAndCal = "Cal and Ricky both like racing.";
 let calRegex = /^Cal/; // Change this line
 let result = calRegex.test(rickyAndCal);
+
+//17 Match Ending String Patterns
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
