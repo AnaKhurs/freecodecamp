17f233ba72e3f298a64ef2e8dd01fad48e2c7091
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 12:22:13 +0300

    Access Property Names with Bracket Notation

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -151,3 +151,21 @@ userActivity.data.online = 45;
 // Only change code above this line
 
 console.log(userActivity);
+
+//14 Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+return foods[scannedItem]
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
