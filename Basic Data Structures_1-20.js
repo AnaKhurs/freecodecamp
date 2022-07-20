2cab0788f611a810dfbbb3ac3428cde5bd858dd5
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 10:11:53 +0300

    Add Items to an Array with push() and unshift()

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -7,3 +7,14 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "n";
 // Only change code above this line
 console.log(myArray);
+
+//3 Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+  // Only change code below this line
+arr.unshift('I', 2, 'three')
+arr.push(7, 'VIII', 9);
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
