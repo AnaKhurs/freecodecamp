c54bf6bb380d901aefb9e13c8c29e70a2fd4ce1e
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 10:19:37 +0300

    Remove Items Using splice()

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -27,3 +27,10 @@ function popShift(arr) {
 }
 
 console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+//5 Remove Items Using splice()
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4);
+// Only change code above this line
+console.log(arr);
