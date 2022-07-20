e163d41a2d2f3444d7f014e4c69712f1336374f1
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:13:13 +0300

    Understanding the Differences between the freeCodeCamp and Browser Console

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -7,3 +7,8 @@ a++;
 console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
+
+//2 Understanding the Differences between the freeCodeCamp and Browser Console
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
