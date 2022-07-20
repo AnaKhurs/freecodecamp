2d4d4112e3953a92e11d915f1e41b9082398fbc4
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:27:37 +0300

    Catch Arguments Passed in the Wrong Order When Calling a Function

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -58,3 +58,13 @@ function getNine() {
 
 let result = getNine();
 console.log(result);
+
+//9 Catch Arguments Passed in the Wrong Order When Calling a Function
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
