7f63d5adb1aea254bf37b8180d544f5171802b39
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:29:59 +0300

    Catch Off By One Errors When Using Indexing

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -68,3 +68,16 @@ let base = 2;
 let exp = 3;
 let power = raiseToPower(base, exp);
 console.log(power);
+
+//10 Catch Off By One Errors When Using Indexing
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  // Only change code below this line
+  for (let i = 0; i < len; i++) {
+  // Only change code above this line
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
