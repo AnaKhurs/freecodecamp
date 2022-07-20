17ed7749cbdb0067a0a631356a655593de3efce4
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:18:28 +0300

    Use typeof to Check the Type of a Variable

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -12,3 +12,11 @@ console.log(sumAB);
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output)
 console.clear()
+
+//3 Use typeof to Check the Type of a Variable
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
