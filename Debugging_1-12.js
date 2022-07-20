bdd478f7a595e8c68ebf7a18d0914e4aa78362f8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:21:30 +0300

    Catch Unclosed Parentheses, Brackets, Braces and Quotes

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -26,3 +26,8 @@ let receivables = 10;
 let payables = 8;
 let netWorkingCapital = receivables - payables;
 console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//5 Catch Unclosed Parentheses, Brackets, Braces and Quotes
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
