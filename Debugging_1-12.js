3deec95f27407c9ae8237a65fc991c97320ef857
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:22:49 +0300

    Catch Mixed Usage of Single and Double Quotes

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -31,3 +31,7 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) =>  previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
+
+//6 Catch Mixed Usage of Single and Double Quotes
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
