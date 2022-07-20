6a110af869ea5e968dcc996e5680419bc6eb274f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 12:13:37 +0300

    Create complex multi-dimensional arrays

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -103,3 +103,20 @@ function filteredArray(arr, elem) {
 }
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+//11 Create complex multi-dimensional arrays
+let myNestedArray = [ //level 1
+  // change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'], //level 2
+  [ 
+    ['loop', 'shift', 6, 7, 1000, 'method', 'deep'], //level 3
+    [ 
+      ['concat', false, true, 'deeper', 'spread', 'array'], //level 4
+      [
+        ['deepest', 'mutate', 1327.98, 'splice', 'slice', 'push'] //level 5
+      ]
+    ]
+  ],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+  // change code above this line
+];
