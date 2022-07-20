ba6040fddfed06e15d08260926f55baf2c064415
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 10:33:23 +0300

    Copy an Array with the Spread Operator

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -54,3 +54,17 @@ arr = arr.slice(2,4)
 
 // Only change code above this line
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//7 Copy an Array with the Spread Operator
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+newArr.push([...arr]);
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
