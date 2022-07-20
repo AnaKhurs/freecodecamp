41fdafce69288c51079822772922df40fa4e0ee2
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 10:21:50 +0300

    Add Items Using splice()

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -34,3 +34,13 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(1,4);
 // Only change code above this line
 console.log(arr);
+
+//6 Add Items Using splice()
+function htmlColorNames(arr) {
+  // Only change code below this line
+arr.splice(0,2,'DarkSalmon', 'BlanchedAlmond')
+  // Only change code above this line
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
