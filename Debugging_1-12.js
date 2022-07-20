2e966dd43fa016dd9aa8c2644a30c0453cb179d8
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:34:48 +0300

    Prevent Infinite Loops with a Valid Terminal Condition

--- Debugging_1-12.js
+++ Debugging_1-12.js
@@ -102,3 +102,10 @@ function zeroArray(m, n) {
 
 let matrix = zeroArray(3, 2);
 console.log(matrix);
+
+//12 Prevent Infinite Loops with a Valid Terminal Condition
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
