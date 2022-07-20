56de777dad2e36411409e6ef2f623d2cca92bf74
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 08:07:43 +0300

    Use the JavaScript Console to Check the Value of a Variable

---
+++ Debugging_1-12.js
@@ -0,0 +1,9 @@
+//1 Use the JavaScript Console to Check the Value of a Variable
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+console.log(a);
+let sumAB = a + b;
+console.log(sumAB);
