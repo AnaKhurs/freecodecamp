0ec663f3cada8db79a61e3e7a534887f11dfc223
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-20 13:13:09 +0300

    Check if an Object has a Property

--- Basic Data Structures_1-20.js
+++ Basic Data Structures_1-20.js
@@ -187,3 +187,35 @@ delete foods.strawberries;
 // Only change code above this line
 
 console.log(foods);
+
+//16 Check if an Object has a Property
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+    if (userObj.hasOwnProperty('Alan') && userObj.hasOwnProperty('Jeff') && userObj.hasOwnProperty('Sarah') && userObj.hasOwnProperty('Ryan')) {
+      return true;
+    } else {
+      return false;
+    }
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
