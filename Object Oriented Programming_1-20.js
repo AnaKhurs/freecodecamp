4d5ecc9636461a58f39c2c112d5850c2cc6eb92c
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 08:06:39 +0300

    Create a Method on an Object

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -12,3 +12,12 @@ let dog = {
 // Only change code below this line
 console.log(dog.name)
 console.log(dog.numLegs)  
+
+//3 Create a Method on an Object
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+sayLegs: function() {return `This dog has ${dog.numLegs} legs.`;}
+};
+
+dog.sayLegs();
