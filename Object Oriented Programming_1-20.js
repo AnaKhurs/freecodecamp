18432d587264ce8533eecb268a9f0759bbe6c039
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 08:51:58 +0300

    Use Prototype Properties to Reduce Duplicate Code

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -79,3 +79,13 @@ for (let property in canary) {
     ownProps.push(property);
   }
 }
+
+//10 Use Prototype Properties to Reduce Duplicate Code
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+// Only change code above this line
+let beagle = new Dog("Snoopy");
