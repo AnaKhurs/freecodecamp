2ac4554c24c7cb031637edfa94a3bcc2fe1e4e33
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 11:29:08 +0300

    Set the Child's Prototype to an Instance of the Parent

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -222,3 +222,20 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype);; // Change this line
 let beagle = Object.create(Animal.prototype);; // Change this line
+
+//19 Set the Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
