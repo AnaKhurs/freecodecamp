09f7595fb07a31644ad58f06f23244446598b0da
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 09:12:46 +0300

    Update Object Oriented Programming_1-20.js

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -141,3 +141,20 @@ eat: function(){
     console.log("My name is " + this.name);
   }
 };
+
+//14 Remember to Set the Constructor Property when Changing the Prototype
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
