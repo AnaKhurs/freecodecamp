33fb85c130f44102213da43adc98c3265249a665
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 09:05:50 +0300

    Change the Prototype to a New Object

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -125,3 +125,19 @@ function joinDogFraternity(candidate) {
     return false;
   }
 }
+
+//13 Change the Prototype to a New Object
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+numLegs:4,
+eat: function(){
+    console.log("nom nom nom");
+},
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
