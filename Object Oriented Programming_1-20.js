d85d6c86d27ad594636d4f585be28cfa7f200e3b
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 08:08:02 +0300

    Make Code More Reusable with the this Keyword

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -21,3 +21,12 @@ sayLegs: function() {return `This dog has ${dog.numLegs} legs.`;}
 };
 
 dog.sayLegs();
+
+//4 Make Code More Reusable with the this Keyword
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
