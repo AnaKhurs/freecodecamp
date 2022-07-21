5839db5543ba5e4ef2934eac0a0f59d80cb16ec5
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 08:53:58 +0300

    Iterate Over All Properties

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -89,3 +89,25 @@ Dog.prototype.numLegs = 4;
 
 // Only change code above this line
 let beagle = new Dog("Snoopy");
+
+//11 Iterate Over All Properties
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+
+for (let property in beagle) {
+  if(beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
