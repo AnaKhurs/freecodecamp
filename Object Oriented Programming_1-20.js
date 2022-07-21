0c2d668d8d8771c57e7303d74c4123d481d66cff
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 13:37:35 +0300

    Use Closure to Protect Properties Within an Object from Being Modified Externally

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -294,3 +294,11 @@ const glideMixin = function (obj){
 }
 glideMixin(bird);
 glideMixin(boat);
+
+//24 Use Closure to Protect Properties Within an Object from Being Modified Externally
+function Bird() {
+  let weight = 15;
+  this.getWeight = function() { 
+    return weight;
+  };
+}
