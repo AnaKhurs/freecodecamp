573b4fef73bc5a2e58b4397a7440a390ecab5d12
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 13:27:04 +0300

    Override Inherited Methods

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -265,3 +265,15 @@ let beagle = new Dog();
 
 beagle.eat();
 beagle.bark();
+
+//22 Override Inherited Methods
+function Bird() { }
+Bird.prototype.fly = function() { return "I am flying!"; };
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+Penguin.prototype.fly = function() {
+  return "Alas, this is a flightless bird.";
+};
+let penguin = new Penguin();
+console.log(penguin.fly());
