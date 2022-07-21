5aab5b1669e36c4eb924c23390132b16653fd0a2
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 10:55:40 +0300

    Use Inheritance So You Don't Repeat Yourself

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -180,3 +180,30 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 
 // Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
+
+//17 Use Inheritance So You Don't Repeat Yourself
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
