0324b7e1479d5992e56ffb0e8a91fefcaff2edc9
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-21 09:03:31 +0300

    Understand the Constructor Property

--- Object Oriented Programming_1-20.js
+++ Object Oriented Programming_1-20.js
@@ -111,3 +111,17 @@ for (let property in beagle) {
     prototypeProps.push(property);
   }
 }
+
+//12 Understand the Constructor Property
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
