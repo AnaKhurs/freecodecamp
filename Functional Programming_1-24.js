64db7042de378354c3e2b1571a6ef6bcf0d4d29b
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-22 11:49:27 +0300

    Understand the Hazards of Using Imperative Code

--- Functional Programming_1-24.js
+++ Functional Programming_1-24.js
@@ -27,3 +27,31 @@ console.log(
   tea4GreenTeamFCC,
   tea4BlackTeamFCC
 );
+
+//3 Understand the Hazards of Using Imperative Code
+
+var Window = function(tabs) {
+  this.tabs = tabs;
+};
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab');
+  return this;
+};
+Window.prototype.tabClose = function (index) {
+  var tabsBeforeIndex = this.tabs.slice(0, index);
+  var tabsAfterIndex = this.tabs.slice(index + 1);
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); 
+  return this;
+ };
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); 
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); 
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']);
+var finalTabs = socialWindow
+  .tabOpen()
+  .join(videoWindow.tabClose(2))
+  .join(workWindow.tabClose(1).tabOpen());
