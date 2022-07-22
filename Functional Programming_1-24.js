9a93be5293cf520eea03163b9520842fc64d0184
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-22 11:27:13 +0300

    Understand Functional Programming Terminology

--- Functional Programming_1-24.js
+++ Functional Programming_1-24.js
@@ -1,5 +1,4 @@
 //1 Learn About Functional Programming
-
 const prepareTea = () => 'greenTea';
 const getTea = (numOfCups) => {
   const teaCups = [];
@@ -10,3 +9,21 @@ const getTea = (numOfCups) => {
   return teaCups;
 };
 const tea4TeamFCC = getTea(40);
+
+//2 Understand Functional Programming Terminology
+const prepareGreenTea = () => 'greenTea';
+const prepareBlackTea = () => 'blackTea';
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4GreenTeamFCC =  getTea( prepareGreenTea, 27);
+const tea4BlackTeamFCC =  getTea(prepareBlackTea, 13);
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
