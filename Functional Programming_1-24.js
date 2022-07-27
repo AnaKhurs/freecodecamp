8a2aac05262aa82c19b6a6d56549292c7f9d4e23
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-27 07:57:59 +0300

    Combine Two Arrays Using the concat Method

--- Functional Programming_1-24.js
+++ Functional Programming_1-24.js
@@ -357,3 +357,11 @@ function nonMutatingSplice(cities) {
 const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 nonMutatingSplice(inputCities);
 
+//13 Combine Two Arrays Using the concat Method
+function nonMutatingConcat(original, attach) {
+  return original.concat(attach)
+  }
+  
+  const first = [1, 2, 3];
+  const second = [4, 5];
+  nonMutatingConcat(first, second);
