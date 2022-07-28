b150a27ddcd0ed4a29249cf2b9365e4f96b857c6
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-07-28 09:50:24 +0300

    Add Elements to the End of an Array Using concat Instead of push

--- Functional Programming_1-24.js
+++ Functional Programming_1-24.js
@@ -365,3 +365,11 @@ function nonMutatingConcat(original, attach) {
   const first = [1, 2, 3];
   const second = [4, 5];
   nonMutatingConcat(first, second);
+
+//14 Add Elements to the End of an Array Using concat Instead of push
+function nonMutatingPush(original, newItem) {
+  return original.concat(newItem);
+}
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
