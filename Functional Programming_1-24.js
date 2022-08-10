056ec091821c0a6e3d7abe375e2ebeb7ace5b33b
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-08-10 08:17:05 +0300

    Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- Functional Programming_1-24.js
+++ Functional Programming_1-24.js
@@ -499,3 +499,16 @@ function getRating(watchList) {
 }
 
 console.log(getRating(watchList));
+
+//16 Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+const squareList = arr => {
+  const positiveIntegers = arr.filter(num => {
+    return num >= 0 && Number.isInteger(num);
+  });
+  const squaredIntegers = positiveIntegers.map(num => {
+    return num ** 2;
+  });
+  return squaredIntegers;
+};
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
