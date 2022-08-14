f8b80fabb01814df3a514a4161ccb2eeda53a0df
AnaKhurs <ana.khurs@gmail.com>
2022-08-14 21:54:47 +0300

    Palindrome Checker

--- JavaScript Algorithms and Data Structures Projects_1-4.js
+++ JavaScript Algorithms and Data Structures Projects_1-4.js
@@ -1,1 +1,11 @@
 //1 Palindrome Checker
+function palindrome(str) {
+    str = str.toLowerCase().replace(/(\W)|(\_)/g,"").split("");
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
+}
+palindrome("eye");
