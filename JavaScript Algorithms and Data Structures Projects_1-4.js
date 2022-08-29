1e2f9f74851d27237934cd8717beda2de8bf2254
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-08-29 10:16:45 +0300

    Roman Numeral Converter

--- JavaScript Algorithms and Data Structures Projects_1-4.js
+++ JavaScript Algorithms and Data Structures Projects_1-4.js
@@ -9,3 +9,31 @@ function palindrome(str) {
     return true;
 }
 palindrome("eye");
+
+//2 Roman Numeral Converter
+function convertToRoman(num) {
+   let roman = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+    let str = '';
+  
+    for (let i of Object.keys(roman)) {
+      let q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+  
+    return str;
+  }
