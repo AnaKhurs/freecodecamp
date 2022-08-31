cdb17d3d0456fa314fc46591019db751f370274c
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-08-31 15:50:33 +0300

    Telephone Number Validator

--- JavaScript Algorithms and Data Structures Projects_1-4.js
+++ JavaScript Algorithms and Data Structures Projects_1-4.js
@@ -59,3 +59,10 @@ function rot13(str) {
   return astr.join('');
   }
   rot13("SERR PBQR PNZC");  
+
+//4 Telephone Number Validator
+let re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+function telephoneCheck(str) {
+  return re.test(str);
+}
+telephoneCheck("555-555-5555");
