f483cdafc094560c07b5e121f76debba1bc30a84
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-09-05 10:06:27 +0300

    Diff Two Arrays

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -9,3 +9,16 @@ function sumAll(arr) {
   return sumBetween;
 }
 sumAll([1, 4]);
+
+//2 Diff Two Arrays
+function diffArray(arr1, arr2) {
+  let newArr = [];  
+  newArr = arr1.concat(arr2); 
+  function checkNum(num) {
+    if (arr1.indexOf(num) === -1 || arr2.indexOf(num) === -1) {
+      return num;
+    }
+  }
+  return newArr.filter(checkNum);
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
