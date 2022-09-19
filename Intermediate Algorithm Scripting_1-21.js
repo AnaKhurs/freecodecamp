b1470911c884ab2af3a7f87c809be97fefe418ae
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-09-19 15:26:14 +0300

    Spinal Tap Case

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -45,3 +45,13 @@ function whatIsInAName(collection, source) {
   });
   return arr;
 }
+
+//5 Spinal Tap Case
+function spinalCase(str) {
+  let spacesRegex = /\s+|_+/g;
+  let camelRegex = /([a-z])([A-Z])/g;
+  str = str.replace(camelRegex, "$1 $2") 
+  let spinalStr = str.replace(spacesRegex, "-");
+  return spinalStr.toLowerCase();
+}
+spinalCase("thisIsSpinalTap");
