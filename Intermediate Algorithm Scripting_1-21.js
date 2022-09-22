0f98c12f5381d51e44fa6697be3fccf1f9b6046d
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2022-09-22 15:14:37 +0300

    Search and Replace

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -79,3 +79,14 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
+
+//8 Search and Replace
+function myReplace(str, before, after) {
+  if (before[0] === before[0].toUpperCase()) {
+      after = after[0].toUpperCase() + after.slice(1);
+  } else {
+      after = after[0].toLowerCase() + after.slice(1);
+  }
+  return str.replace(before, after);
+}
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
