7f34aeff29da032979427b29dd1190ec7ebd6cc5
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2023-01-16 13:25:05 +0300

    Binary Agents

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -253,3 +253,10 @@ function steamrollArray(ourArray) {
   console.log(emptyFlatArray);
   return emptyFlatArray;
 }
+
+//19 Binary Agents
+function binaryAgent(str) {
+  return str.split(' ').map(function(s) { return parseInt(s, 2); }).map(function(b) { return String.fromCharCode(b);}).join('');
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
