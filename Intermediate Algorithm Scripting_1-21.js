6f7d3ab9aa58174fb5cfd9249467e5a5d9297b3f
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2023-01-16 13:50:20 +0300

    Map the Debris

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -307,3 +307,20 @@ const Person = function(firstAndLast) {
 
 const bob = new Person("Bob Ross");
 console.log(bob.getFullName());
+
+//23 Map the Debris
+function orbitalPeriod(arr) {
+  let GM = 398600.4418;
+  let earthRadius = 6367.4447;
+  return arr.map(satellite => {
+    let alt=satellite.avgAlt;
+    delete satellite.avgAlt;
+    satellite["orbitalPeriod"] = Math.round(2 * Math.PI * Math.sqrt( Math.pow((alt+earthRadius),3) / GM));
+    return satellite;
+  });
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]); // [{name: "sputnik", orbitalPeriod: 86400}]
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, 
+{name: "hubble", avgAlt: 556.7}, 
+{name: "moon", avgAlt: 378632.553}]); // [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
