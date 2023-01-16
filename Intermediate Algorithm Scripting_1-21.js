3c38c9535ea19a8676057580b2e4cc37a71e562b
AnaKhurs <55126430+AnaKhurs@users.noreply.github.com>
2023-01-16 13:14:41 +0300

    Smallest Common Multiple

--- Intermediate Algorithm Scripting_1-21.js
+++ Intermediate Algorithm Scripting_1-21.js
@@ -202,3 +202,27 @@ function sumPrimes(num) {
   return primesum;
 }
 sumPrimes(10);
+
+//16 Smallest Common Multiple
+function smallestCommons(arr) {
+  let max;
+  let min;
+if (arr[0] > arr[1]) {
+    max = arr[0];
+    min = arr[1];
+  } else {
+    min = arr[0];
+    max = arr[1];
+  }
+let multiple = max;
+  for (let i = min; i < max; i++) {
+    if (multiple % i !== 0) {
+      multiple += max;
+      i = min - 1;
+    } else if (i === max) {
+      return multiple;
+    }
+  }
+  return multiple;
+}
+smallestCommons([1,5]);
